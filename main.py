0f18e2e5541b6d44aaea6b54d2a6fcbdc4c754dc
Fabien Danieau <fabien.danieau@technicolor.com>
2020-04-22 17:02:10 +0200

    add validation data to train function add test function to compute the test loss

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data.sampler import SubsetRandomSampler
 
 
 def show_pictures(loader, nb_imgs=0, model=None, batch_size=20):
@@ -33,7 +34,16 @@ def show_pictures(loader, nb_imgs=0, model=None, batch_size=20):
     plt.show()
 
 
-def prepare_data(data_root_path, batch_size):
+def split_indices_train(train_data, valid_size):
+    num_train = len(train_data)
+    indices = list(range(num_train))
+    np.random.shuffle(indices)
+    split = int(np.floor(valid_size * num_train))
+    train_idx, valid_idx = indices[split:], indices[:split]
+    return train_idx, valid_idx
+
+
+def prepare_data(data_root_path, batch_size, valid_size=0.2):
 
     transform = transforms.Compose([
         transforms.Grayscale(),
@@ -42,7 +52,7 @@ def prepare_data(data_root_path, batch_size):
         transforms.ToTensor(),
     ])
 
-    transformtest = transforms.Compose([  
+    transformtest = transforms.Compose([
         transforms.Grayscale(),
         transforms.Resize((64, 64)),
         transforms.ToTensor(),
@@ -57,97 +67,101 @@ def prepare_data(data_root_path, batch_size):
     # Create training and test dataloaders
     num_workers = 0
 
+    train_idx, valid_idx = split_indices_train(test_data, valid_size)
+    test_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
     # prepare data loaders
     train_loader = torch.utils.data.DataLoader(train_data,
                                                batch_size=batch_size,
                                                shuffle=True,
                                                num_workers=num_workers)
+    valid_loader = torch.utils.data.DataLoader(train_data,
+                                               batch_size=batch_size,
+                                               sampler=valid_sampler,
+                                               num_workers=num_workers)
+
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
-                                              shuffle=True,
+                                              sampler=test_sampler,
                                               num_workers=num_workers)
 
-    return train_loader, test_loader
+    return train_loader, valid_loader, test_loader
+
 
 class ConvDenoiser(nn.Module):
     def __init__(self):
         super(ConvDenoiser, self).__init__()
-        ## encoder layers ##
+        # encoder layers #
         # conv layer (depth from 1 --> 32), 3x3 kernels
-        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)  
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
         # conv layer (depth from 32 --> 16), 3x3 kernels
         self.conv2 = nn.Conv2d(32, 16, 3, padding=1)
         # conv layer (depth from 16 --> 8), 3x3 kernels
         self.conv3 = nn.Conv2d(16, 8, 3, padding=1)
         # pooling layer to reduce x-y dims by two; kernel and stride of 2
         self.pool = nn.MaxPool2d(2, 2)
-        
-        ## decoder layers ##
-        # transpose layer, a kernel of 2 and a stride of 2 will increase the spatial dims by 2
-        self.t_conv1 = nn.ConvTranspose2d(8, 8, 2, stride=2)  # kernel_size=3 to get to a 7x7 image output
+
+        # decoder layers #
+        # transpose layer, a kernel of 2 and a stride of 2
+        # will increase the spatial dims by 2
+        self.t_conv1 = nn.ConvTranspose2d(8, 8, 2, stride=2)
         # two more transpose layers with a kernel of 2
         self.t_conv2 = nn.ConvTranspose2d(8, 16, 2, stride=2)
         self.t_conv3 = nn.ConvTranspose2d(16, 32, 2, stride=2)
         # one, final, normal conv layer to decrease the depth
         self.conv_out = nn.Conv2d(32, 1, 3, padding=1)
-        
-        #self.dropout = nn.Dropout(0.2)
 
+        # self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
-        ## encode ##
+        # encode #
         # add hidden layers with relu activation function
         # and maxpooling after
         x = F.relu(self.conv1(x))
         x = self.pool(x)
-        #x = self.dropout(x)
+        # x = self.dropout(x)
         # add second hidden layer
         x = F.relu(self.conv2(x))
         x = self.pool(x)
-        #x = self.dropout(x)
+        # x = self.dropout(x)
         # add third hidden layer
         x = F.relu(self.conv3(x))
         x = self.pool(x)  # compressed representation
-        #x = self.dropout(x)
-        
-        ## decode ##
+        # x = self.dropout(x)
+
+        # decode #
         # add transpose conv layers, with relu activation function
         x = F.relu(self.t_conv1(x))
-        #x = self.dropout(x)
+        # x = self.dropout(x)
         x = F.relu(self.t_conv2(x))
-        #x = self.dropout(x)
+        # x = self.dropout(x)
         x = F.relu(self.t_conv3(x))
-        #x = self.dropout(x)
+        # x = self.dropout(x)
         # transpose again, output should have a sigmoid applied
         x = torch.sigmoid(self.conv_out(x))
-                
+      
         return x
 
-def train(model, train_loader, n_epochs = 50, noise_factor=0.5, device="cpu", save_path = None):
+
+def train(model, train_loader, valid_loader, n_epochs=50, noise_factor=0.5,
+          device="cpu", save_path=None):
+
+    min_valid_loss = np.inf
 
     for epoch in range(1, n_epochs+1):
         # monitor training loss
         train_loss = 0.0
-        min_train_loss = np.inf
+        valid_loss = 0.0
 
+        model.train() 
         for data in train_loader:
-            # _ stands in for labels, here
-            # no need to flatten images
             images, _ = data
-        
-            ## add random noise to the input images
-            # noisy_imgs = images + noise_factor * torch.randn(*images.shape)
-            # Clip the images to be between 0 and 1
-            # noisy_imgs = np.clip(noisy_imgs, 0., 1.)
-            # noisy_imgs = noisy_imgs.to(device)
-                
+                        
             # clear the gradients of all optimized variables
             optimizer.zero_grad()
-            ## forward pass: compute predicted outputs by passing *noisy* images to the model
-            #outputs = model(noisy_imgs)
             images = images.to(device)
             outputs = model(images)
-            # calculate the loss
-            # the "target" is still the original, not-noisy images        
+            # calculate the loss     
             loss = criterion(outputs, images)
             # backward pass: compute gradient of the loss with respect to model parameters
             loss.backward()
@@ -156,19 +170,46 @@ def train(model, train_loader, n_epochs = 50, noise_factor=0.5, device="cpu", sa
             # update running training loss
             train_loss += loss.item()*images.size(0)
             
-        # print avg training statistics 
-        train_loss = train_loss/len(train_loader)
+        model.eval()  # prep model for evaluation
+        for images, _ in valid_loader:
+            # forward pass: compute predicted outputs by passing inputs to the model
+            images = images.to(device)
+            output = model(images)
+            #  calculate the loss
+            loss = criterion(output, images)
+            # update running validation loss 
+            valid_loss += loss.item()*images.size(0)
+        
+        # print avg training statistics
+        train_loss = train_loss/len(train_loader.sampler)
+        valid_loss = valid_loss/len(valid_loader.sampler)
         save_model = ""
-        if(train_loss < min_train_loss and save_path is not None):
+        if(valid_loss < min_valid_loss and save_path is not None):
             save_model = " (model saved)"
             torch.save(model.state_dict(), save_path)
-            min_train_loss = train_loss
+            min_valid_loss = train_loss
 
         print('Epoch: {} \tTraining Loss: {:.6f}'.format(
-            epoch, 
-            train_loss
-            )+save_model)
+            epoch, train_loss)
+            + ' Valid loss {:.6f}: '.format(valid_loss)
+            + save_model)
 
+
+def test(model, test_loader, device="cpu"):
+    test_loss = 0.0
+    model.eval()  # prep model for evaluation
+    for images, _ in valid_loader:
+        # forward pass: compute predicted outputs by passing inputs to the model
+        images = images.to(device)
+        output = model(images)
+        #  calculate the loss
+        loss = criterion(output, images)
+        # update running test loss 
+        test_loss += loss.item()*images.size(0)
+
+    # calculate and print avg test loss
+    test_loss = test_loss/len(test_loader.sampler)
+    print('Test Loss: {:.6f}\n'.format(test_loss))
 
 
 if __name__ == '__main__':
@@ -188,7 +229,7 @@ if __name__ == '__main__':
     batch_size = 20
 
     data_root_path = "/home/danieauf/Data/iris"
-    train_loader, test_loader = prepare_data(data_root_path, batch_size)
+    train_loader, valid_loader, test_loader = prepare_data(data_root_path, batch_size)
 
     #show_pictures(train_loader, 10)
 
@@ -201,14 +242,17 @@ if __name__ == '__main__':
 
     model_path = "trained_model.pt"
 
-    #if is_cuda:
-    #    model.cuda()
-    #train(model, train_loader, device=dev, save_path=model_path)
+    if is_cuda:
+        model.cuda()
+    #train(model, train_loader, valid_loader, device=dev, save_path=model_path)
 
     model.load_state_dict(torch.load(model_path))
+
+    if is_cuda:
+        model.cuda()
+    test(model, test_loader, device=dev)
 
     if(is_cuda):
         model.cpu()
 
     show_pictures(test_loader, 10, model, batch_size)
-    
